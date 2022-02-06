f37fea6e9523090a29750b971cd72b3ebb1f43fe
MurtazaNeher <96943172+MurtazaNeher@users.noreply.github.com>
2022-02-06 16:54:24 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,6 @@
+var billAmount = document.querySelector("#bill-amount");
+var cashGiven = document.querySelector("#cash-given");
+var checkButton = document.querySelector("#check-button");
+checkButton.addEventListener("click", console.log("clicked"))
+
+console.log(cashGiven.value);
